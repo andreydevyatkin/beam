c1e4191ad37c5e03a12496aca557c06d20435cd4
Andrey Devyatkin <andrey.9evyatkin@gmail.com>
2023-11-01 22:03:07 +0100

    jacoco plugin test

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1343,14 +1343,14 @@ class BeamModulePlugin implements Plugin<Project> {
           description = "Generates code coverage report for SQL related classes"
           
           println "current project: ${project}"
-          getClassDirectories().setFrom(project.fileTree(
-                    dir: project.buildDir,
+          getClassDirectories().setFrom(project.files(project.fileTree(
+                    dir: project.rootDir,
                     includes: project.hasProperty('jacocoIncludes') ? project.property('jacocoIncludes').split(',') as List<String> : configuration.jacocoIncludes,
                     excludes: project.hasProperty('jacocoExcludes') ? project.property('jacocoExcludes').split(',') as List<String> : configuration.jacocoExcludes
-          ))
-          getAdditionalSourceDirs().setFrom(project.files(project.sourceSets.main.allSource.srcDirs))
-          getSourceDirectories().setFrom(project.files(project.sourceSets.main.allSource.srcDirs))
-          getExecutionData().setFrom(project.files(project.files("${project.buildDir}/jacoco/test.exec")))
+          )))
+          getAdditionalSourceDirs().setFrom(project.sourceSets.main.allSource.srcDirs)
+          getSourceDirectories().setFrom(project.sourceSets.main.allSource.srcDirs)
+          getExecutionData().setFrom(project.fileTree(project.buildDir).include("/jacoco/*.exec"))
           project.subprojects.each { subproject ->
             subproject.tasks.withType(JacocoReport).each { report ->
                 println "subproject task: ${report}"
