c4142bd526ab64c285fc32259f4baed7818d449e
Andrey Devyatkin <andrey.9evyatkin@gmail.com>
2023-11-02 02:03:49 +0100

    jacoco plugin test

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1336,50 +1336,35 @@ class BeamModulePlugin implements Plugin<Project> {
       //   // }
       // }
 
-      project.jacocoTestReport {
-        doFirst {
-          getClassDirectories().setFrom(project.files(
-              project.fileTree(
-                dir: project.rootDir,
-                includes: project.hasProperty('jacocoIncludes') ? project.property('jacocoIncludes').split(',') as List<String> : configuration.jacocoIncludes,
-                excludes: project.hasProperty('jacocoExcludes') ? project.property('jacocoExcludes').split(',') as List<String> : configuration.jacocoExcludes
-          )))
-        }
-        reports {
-          xml.required = true
-          html.required = true
+      project.afterEvaluate {
+        project.jacocoTestReport {
+          // dependsOn project.test
+          group = "Reporting"
+          description = "Generates code coverage report for SQL related classes"
+          
+          println "current project: ${project}"
+          getClassDirectories().setFrom(project.files(project.files(project.sourceSets.main.output).collect {
+                project.fileTree(
+                        dir: it,
+                        includes: project.hasProperty('jacocoIncludes') ? project.property('jacocoIncludes').split(',') as List<String> : configuration.jacocoIncludes,
+                        excludes: project.hasProperty('jacocoExcludes') ? project.property('jacocoExcludes').split(',') as List<String> : configuration.jacocoExcludes)
+          }))
+          getAdditionalSourceDirs().setFrom(project.sourceSets.main.allSource.srcDirs)
+          getSourceDirectories().setFrom(project.sourceSets.main.allSource.srcDirs)
+          getExecutionData().setFrom(project.fileTree(project.buildDir).include("/jacoco/*.exec"))
+          project.subprojects.each { subproject ->
+            subproject.tasks.withType(JacocoReport).each { report ->
+                println "subproject task: ${report}"
+                getAdditionalClassDirs().setFrom(report.getAllClassDirs())
+                getAdditionalSourceDirs().setFrom(report.getAllSourceDirs())
+            }
+          }
+          reports {
+            xml.required = true
+            html.required = true
+          }
         }
       }
-
-      // project.afterEvaluate {
-      //   project.jacocoTestReport {
-      //     // dependsOn project.test
-      //     group = "Reporting"
-      //     description = "Generates code coverage report for SQL related classes"
-          
-      //     println "current project: ${project}"
-      //     getClassDirectories().setFrom(project.files(project.files(project.sourceSets.main.output).collect {
-      //           project.fileTree(
-      //                   dir: it,
-      //                   includes: project.hasProperty('jacocoIncludes') ? project.property('jacocoIncludes').split(',') as List<String> : configuration.jacocoIncludes,
-      //                   excludes: project.hasProperty('jacocoExcludes') ? project.property('jacocoExcludes').split(',') as List<String> : configuration.jacocoExcludes)
-      //     }))
-      //     getAdditionalSourceDirs().setFrom(project.sourceSets.main.allSource.srcDirs)
-      //     getSourceDirectories().setFrom(project.sourceSets.main.allSource.srcDirs)
-      //     getExecutionData().setFrom(project.fileTree(project.buildDir).include("/jacoco/*.exec"))
-      //     project.subprojects.each { subproject ->
-      //       subproject.tasks.withType(JacocoReport).each { report ->
-      //           println "subproject task: ${report}"
-      //           getAdditionalClassDirs().setFrom(report.getAllClassDirs())
-      //           getAdditionalSourceDirs().setFrom(report.getAllSourceDirs())
-      //       }
-      //     }
-      //     reports {
-      //       xml.required = true
-      //       html.required = true
-      //     }
-      //   }
-      // }
 
       // project.jacocoTestReport {
       //   doFirst {
